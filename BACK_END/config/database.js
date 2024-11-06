d8a92f2b7a4096a98428159208a9a23a7d0f6881
ZainBakor <zeyneldin2002@gmail.com>
2024-11-07 00:22:19 +0300

    connected to postgres

--- BACK_END/config/database.js
+++ BACK_END/config/database.js
@@ -1,1 +1,21 @@
-// TODO
+//  Initialize the pg package and get the Client from it.
+const { Client } = require('pg');
+
+//Database connection configuration
+const client = new Client({
+	user: 'postgres',
+	password: 'postgres',
+	host: 'localhost',
+	port: 5432,
+	database: 'staj-2',
+});
+//Connect to the database
+client
+.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+});
+
