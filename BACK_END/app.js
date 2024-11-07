fc492debbdbb309d3b2dd2a86c6aa706fc8d12c8
Ahmad Shamsddin <ahmadshamsddin@gmail.com>
2024-11-07 19:37:59 +0300

    update port number

--- BACK_END/app.js
+++ BACK_END/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   next(error);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`API listening on port ${PORT}`);
